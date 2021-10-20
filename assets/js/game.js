a1831b26d6a2221c8e3ca5fc67bf48be0f65263b
Katelyn Bunker <kerbunker@gmail.com>
2021-10-20 18:28:39 -0400

    added high scores

--- assets/js/game.js
+++ assets/js/game.js
@@ -141,11 +141,21 @@ var startGame = function() {
 var endGame = function() {
   window.alert("The game has now ended. Let's see how you did!");
 
-  // if player is still alive, player wins!
-  if (playerInfo.health > 0) {
-    window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + '.');
+  //check localStorage for hight score, if it's not there use 0
+  var highScore = localStorage.getItem("highscore");
+  if (highscore === null) {
+    highscore = 0;
+  }
+
+  // if player has more money than the high score, the player has new high score!
+  if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(playerInfo.name + "now has the high score of " + playerInfo.money + "!");
+
   } else {
-    window.alert("You've lost your robot in battle!");
+    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
   }
 
   // ask player if they'd like to play again
